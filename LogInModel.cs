ba116010e144c7fa700e67d76e1e1f1ea18245df
DHAVALBHAI RANA <142986000+DhavalRana11@users.noreply.github.com>
2024-05-23 13:01:47 +0100

    Add files via upload

---
+++ LogInModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuickShoap.Models
+{
+    public class LogInModel
+    {
+        /// <summary>
+        /// Get or Sets strPassword value
+        /// </summary>
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password:")]
+        [StringLength(50)]
+        public string strPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets the StrEmail value.
+        /// </summary>
+       
+        public string strCompanyCode
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the StrEmail value.
+        /// </summary>
+        [Required(ErrorMessage = "*")]
+        public string strLoginName
+        {
+            get;
+            set;
+        }
+    }
+}
